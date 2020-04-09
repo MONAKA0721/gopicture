24dd7578ee7d25e078cc43cbd1870c5a89da230f
山本浩平 <>
2020-04-09 19:59:18 +0900

    クリックで画像ポップアップ表示機能実装

--- statics/js/show.js
+++ statics/js/show.js
@@ -5,7 +5,6 @@ $(function(){
   $("span").each(function(e,v){
       spans.push(v);
     });
-    console.log(spans);
     for (let i=0; i < spans.length; i++){
       favNum.push(Number(spans[i].innerText));
     }
@@ -15,8 +14,6 @@ $(function(){
       }
     );
 
-    console.log(favNum[0]);
-    console.log(spans[0].innerText);
     function GetNum1(){
       for (let i=0; i < spans.length; i++){
         if(favNum[0] == spans[i].innerText){
@@ -52,31 +49,25 @@ $(function(){
         }
       }
     }
-    console.log(favNum);
 
     if(spans[GetNum1()]){
       $('#' + `${spans[GetNum1()].id}`).parents('.picture').attr('style','grid-row:1/2;grid-column:1/4;');
-      console.log(spans[GetNum1()].id);
       spans.splice(GetNum1(),1);
     }
     if(spans[GetNum2()]){
       $('#' + `${spans[GetNum2()].id}`).parents('.picture').attr('style','grid-row:2/3;grid-column:1/3;');
-      console.log(spans[GetNum2()].id);
       spans.splice(GetNum2(),1);
     }
     if(spans[GetNum3()]){
       $('#' + `${spans[GetNum3()].id}`).parents('.picture').attr('style','grid-row:2/3;grid-column:3/4;');
-      console.log(spans[GetNum3()].id);
       spans.splice(GetNum3(),1);
     }
     if(spans[GetNum4()]){
       $('#' + `${spans[GetNum4()].id}`).parents('.picture').attr('style','grid-row:3/4;grid-column:1/2;');
-      console.log(spans[GetNum4()].id);
       spans.splice(GetNum4(),1);
     }
     if(spans[GetNum5()]){
       $('#' + `${spans[GetNum5()].id}`).parents('.picture').attr('style','grid-row:3/4;grid-column:2/4;');
-      console.log(spans[GetNum5()].id);
       spans.splice(GetNum5(),1);
     }
 
@@ -98,3 +89,24 @@ $(function(){
     location.reload();
   });
 });
+
+
+function popupImage() {
+  var popup = document.getElementById('js-popup');
+  if(!popup) return;
+
+  $('.js-show-popup').on("click", function(){
+    var imgsrc = $(this).attr('src');
+    $('.popup-inner').find('img').attr('src',imgsrc);
+    popup.classList.toggle('is-show');
+  });
+
+  $('#js-close-btn').on("click", function(){
+    popup.classList.toggle('is-show');
+  });
+
+  $('#js-black-bg').on("click", function(){
+    popup.classList.toggle('is-show');
+  });
+}
+popupImage();
